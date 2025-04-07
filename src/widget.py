b894d1db287a561e1e040afa2b3fef96a7ead252
Anton-Z <rockangel520@mail.ru>
2025-04-07 14:50:05 +0500

    Создана корректная функция get_date

--- src/widget.py
+++ src/widget.py
@@ -11,3 +11,9 @@ def mask_account_card(card_or_account_number: str) -> str:
         card_name_and_numder[-1] = masks.get_mask_card_number(card_name_and_numder[-1])
         mask_account_number = " ".join(card_name_and_numder)
         return mask_account_number
+
+
+def get_date(input_date: str) -> str:
+    """Функция, которая преобразует дату в формат ДД.ММ.ГГГГ"""
+    output_date = f"{input_date[8:10]}.{input_date[5:7]}.{input_date[0:4]}"
+    return output_date
