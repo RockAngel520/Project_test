b0fc7c0187778c70071e77059169cbec4249144c
Anton-Z <rockangel520@mail.ru>
2025-04-16 21:13:05 +0500

    create func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def filter_by_state(list_of_dict: list[dict], state: str ='EXECUTED') -> list[dict]:
+from typing import Dict, List
+
+
+def filter_by_state(list_of_dict: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """Фильтрация списка по ключу 'state'"""
     new_list_of_dict = []
     for dict_ in list_of_dict:
@@ -6,3 +9,8 @@ def filter_by_state(list_of_dict: list[dict], state: str ='EXECUTED') -> list[di
             new_list_of_dict.append(dict_)
 
     return new_list_of_dict
+
+
+def sort_by_date(list_of_dict: List[Dict], reverse_date: bool = True) -> List[Dict]:
+    """Функция сортировки по дате"""
+    return sorted(list_of_dict, key=lambda date: date['date'], reverse=reverse_date)
