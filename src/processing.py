d4434e21e144b6a26baf97ebb3dce05e17f456a1
Anton-Z <rockangel520@mail.ru>
2025-04-16 23:19:23 +0500

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
 from typing import Dict, List
 
 
-def filter_by_state(list_of_dict: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(list_of_dict: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Фильтрация списка по ключу 'state'"""
     new_list_of_dict = []
     for dict_ in list_of_dict:
-        if dict_.get('state') == state:
+        if dict_.get("state") == state:
             new_list_of_dict.append(dict_)
 
     return new_list_of_dict
@@ -13,4 +13,4 @@ def filter_by_state(list_of_dict: List[Dict], state: str = 'EXECUTED') -> List[D
 
 def sort_by_date(list_of_dict: List[Dict], reverse_date: bool = True) -> List[Dict]:
     """Функция сортировки по дате"""
-    return sorted(list_of_dict, key=lambda date: date['date'], reverse=reverse_date)
+    return sorted(list_of_dict, key=lambda date: date["date"], reverse=reverse_date)
